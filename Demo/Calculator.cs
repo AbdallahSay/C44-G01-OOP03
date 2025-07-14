1b4b11f6dcb074815fa9b17d35b20d1cf213e61e
AbdallahSay <luka3122003@gmail.com>
2025-07-14 21:28:27 +0300

    Q1

---
+++ Demo/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Calculator
+    {
+        //public int X { get; set; }
+        //public int y { get; set; }
+        //public int Z { get; set; }
+        //public decimal A { get; set; }
+        //public decimal B { get; set; }
+        public int SumNumbers(int x , int y)
+        {
+            return x + y;
+        }
+        public int SumNumbers(int x , int y , int z)
+        {
+            return x + y + z;
+        }
+        public double SumNumbers(double x , double y)
+        {
+            return x + y;
+        }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Demo
+﻿namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            #region Q1) Write a class named Calculator that contains a method named Add. Overload the Add method to: 
+            //Accept two integers and return their sum. 
+            //Calculator calc = new Calculator();
+            //int x = 5 ,y = 10 , z = 20 ;
+            //double a = 5.5, b = 2.5;
+            //Console.WriteLine( calc.SumNumbers(x, y));
+            ////Accept three integers and return their sum. 
+            //Console.WriteLine( calc.SumNumbers(x, y , z));
+            ////Accept two double and return their sum.
+            //Console.WriteLine( calc.SumNumbers(a ,b));
+
+
+            #endregion
         }
     }
 }
