eac72fed50a622a4e7a89a1c63e5ff96f6b17789
AbdallahSay <luka3122003@gmail.com>
2025-07-14 13:08:00 +0300

    Method Overloading

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,9 +2,44 @@
 {
     internal class Program
     {
+        #region Method Overloading
+        public static int SumNumbers(int x, int y)
+        {
+            return x + y;
+        }
+        public static double SumNumbers(double x, int y)
+        {
+            return x + y;
+        }
+        public static double SumNumbers(double x, double y)
+        {
+            return x + y;
+        }
+        public static int SumNumbers(int x, int y, int z)
+        {
+            return x + y;
+        }
+
+        public static double SumNumbers(double x, double y, double z)
+        {
+            return x + y;
+        }
+
+
+        #endregion
+
         static void Main(string[] args)
         {
-           
+            #region Method Overloading
+            int x = 10, y = 20, z = 30;
+            double a = 1.5, b = 3.5, c = 8.5;
+            SumNumbers(x, y);
+            SumNumbers(a, y);
+            SumNumbers(a, b);
+            SumNumbers(a, b, c);
+            SumNumbers(x, y, z);
+            #endregion
+
         }
     }
 }
