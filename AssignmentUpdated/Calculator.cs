eff5c554a930dabd7c5259491901188984c64f7f
AbdallahSay <luka3122003@gmail.com>
2025-07-14 21:40:26 +0300

    Q1

---
+++ AssignmentUpdated/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentUpdated
+{
+    internal class Calculator
+    {
+        public int SumNumbers(int x, int y)
+        {
+            return x + y;
+        }
+        public int SumNumbers(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+        public double SumNumbers(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
